322cd01e8ef810249061f6772a10f72022fab84b
Victor-svg <victorsavall@hotmail.fr>
2020-11-06 16:02:57 +0100

    Admin terminé

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -11,15 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiPostController extends AbstractController
 {
     /**
-     * @Route("/api/post", name="api_post_index", methods={"GET"})
+     * @Route("/api/articles", name="api_articles_index", methods={"GET"})
      */
     public function index(ArticleRepository $articleRepository): Response
     {
-        $article = $articleRepository->findAll();
-        dd($article);
-
-        return $this->render('api_post/index.html.twig', [
-            'controller_name' => 'ApiPostController',
-        ]);
+        return $this->json($articleRepository->findAll(), 200, []);
     }
 }
